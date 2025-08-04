8eb97b77415df6636b8b47c00636a4f1fd05211b
Kartikkumar04 <kartikkumarxx4@gmail.com>
2025-08-04 10:35:14 +0530

    .

--- PYTHONCODEWITHHARRY/CHAPTER-4/1list.py
+++ PYTHONCODEWITHHARRY/CHAPTER-4/1list.py
@@ -1,1 +1,9 @@
-#PYTHON LISTES ARE 
+# In Lists we can store any type of data.
+
+a = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(a[2])
+
+
+b = ["CODE", "DEVELOP","ER","04"]
+b[2] = "ERS"
+print(b)
