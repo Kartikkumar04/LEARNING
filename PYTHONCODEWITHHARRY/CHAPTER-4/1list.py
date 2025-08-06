11ee30e3ce769b03074e526658fba5a890883dea
Kartikkumar04 <kartikkumarxx4@gmail.com>
2025-08-06 09:05:00 +0530

    .

--- PYTHONCODEWITHHARRY/CHAPTER-4/1list.py
+++ PYTHONCODEWITHHARRY/CHAPTER-4/1list.py
@@ -1,9 +1,14 @@
 # In Lists we can store any type of data.
 
 a = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
-print(a[2])
+print(a,a[2])
 
+# Lists are mutable meaning we can change their values.
 
 b = ["CODE", "DEVELOP","ER","04"]
 b[2] = "ERS"
 print(b)
+
+# In lists we can also perform slicing like strings
+
+print(a[1:4])   
